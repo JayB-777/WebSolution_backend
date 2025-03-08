e7c3b8d733c760b67e8731031661bd56d6dce6e7
quswjdgns399 <quswjdgns3996@gmail.com>
2025-03-09 00:37:37 +0900

    feat: GlobalExceptionHandler 추가 및 예외 처리 일원화

--- src/main/java/com/websolution/api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/websolution/api/common/exception/GlobalExceptionHandler.java
@@ -1,2 +1,17 @@
-package com.websolution.api.common.exception;public class GlobalExceptionHandler {
+package com.websolution.api.common.exception;
+
+import com.websolution.api.common.response.BaseResponse;
+import com.websolution.api.common.response.BaseResponseStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // RuntimeException 예외 처리
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<BaseResponse<String>> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(new BaseResponse<>(BaseResponseStatus.BAD_REQUEST, ex.getMessage()));
+    }
 }
