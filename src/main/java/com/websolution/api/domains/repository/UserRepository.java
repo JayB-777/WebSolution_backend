1d9f963625de89be2345b93277ff668efdb83be3
quswjdgns399 <quswjdgns3996@gmail.com>
2025-03-09 00:41:15 +0900

    feat: loginId, email 중복 검사 메서드 추가

--- src/main/java/com/websolution/api/domains/repository/UserRepository.java
+++ src/main/java/com/websolution/api/domains/repository/UserRepository.java
@@ -8,4 +8,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLoginId(String loginId);
+
+    boolean existsByLoginId(String loginId); // loginId 중복 검사
+
+    boolean existsByEmail(String email); // email 중복 검사
 }
