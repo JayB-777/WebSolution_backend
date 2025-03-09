32a05b38f4f30937f9d851bca0281eac57e84328
quswjdgns399 <quswjdgns3996@gmail.com>
2025-03-09 16:05:15 +0900

    🚀 refactor:  DTO 추가

--- src/main/java/com/websolution/api/domains/admin/model/response/PendingUserResponse.java
+++ src/main/java/com/websolution/api/domains/admin/model/response/PendingUserResponse.java
@@ -1,2 +1,22 @@
-package com.websolution.api.domains.admin.model.response;public class PendingUserResponse {
+package com.websolution.api.domains.admin.model.response;
+
+import com.websolution.api.domains.entity.Role;
+import com.websolution.api.domains.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PendingUserResponse {
+    private String loginId;
+    private String userName;
+    private String email;
+    private String phoneNumber;
+    private String company;
+    private Role role; // 기본적으로 PENDING 상태
+
+    public static PendingUserResponse fromEntity(User user) {
+        return new PendingUserResponse(user.getLoginId(), user.getUserName(), user.getEmail(), user.getPhoneNumber(),
+                user.getCompany(), user.getRole());
+    }
 }
