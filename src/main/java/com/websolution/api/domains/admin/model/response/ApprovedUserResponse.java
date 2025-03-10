866b4344beb368212c817c92a329a24988095e7f
quswjdgns399 <quswjdgns3996@gmail.com>
2025-03-10 23:35:12 +0900

    Revert "feat: pending 상태의 유저 승인을 위한 RequestDto 구현"
    This reverts commit ed230a9ba4f016fe63eb15d08d3cbbc3d0e575fb.

--- src/main/java/com/websolution/api/domains/admin/model/response/ApprovedUserResponse.java
+++ src/main/java/com/websolution/api/domains/admin/model/response/ApprovedUserResponse.java
@@ -1,17 +1,5 @@
 package com.websolution.api.domains.admin.model.response;
 
-import com.websolution.api.domains.entity.Role;
-import com.websolution.api.domains.entity.User;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class ApprovedUserResponse {
+public class AppovedUesrResponse {
     private String loginId;
-    private Role role;
-
-    public static ApprovedUserResponse fromEntity(User user) {
-        return new ApprovedUserResponse(user.getLoginId(), user.getRole());
-    }
 }
