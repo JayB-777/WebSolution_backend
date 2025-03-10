67704ed1eb20a4b07a21435d6eaacb0cb0cb586f
quswjdgns399 <quswjdgns3996@gmail.com>
2025-03-10 23:47:02 +0900

    feat: pending 상태의 유저 승인을 위한 RequestDto 구현

--- src/main/java/com/websolution/api/domains/admin/model/request/ApproveUsersRequest.java
+++ src/main/java/com/websolution/api/domains/admin/model/request/ApproveUsersRequest.java
@@ -1,2 +1,11 @@
-package com.websolution.api.domains.admin.model.request;public class ApproveUsersRequest {
+package com.websolution.api.domains.admin.model.request;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ApproveUsersRequest {
+    private List<String> userLoginIds;
 }
