7a92ad3911b948547b34941578ae63f0a7a85395
quswjdgns399 <quswjdgns3996@gmail.com>
2025-03-11 00:01:10 +0900

    refactor: 승인된 사용자 목록을 반환하도록 수정

--- src/main/java/com/websolution/api/domains/admin/controller/AdminController.java
+++ src/main/java/com/websolution/api/domains/admin/controller/AdminController.java
@@ -1,14 +1,15 @@
 package com.websolution.api.domains.admin.controller;
 
 import com.websolution.api.common.response.BaseResponse;
+import com.websolution.api.domains.admin.model.request.ApproveUsersRequest;
+import com.websolution.api.domains.admin.model.response.ApprovedUserResponse;
 import com.websolution.api.domains.admin.model.response.PendingUserResponse;
 import com.websolution.api.domains.admin.service.AdminLoginService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,14 +21,14 @@ public class AdminController {
     private final AdminLoginService adminService;
 
     @GetMapping("/pending-users")
-    public ResponseEntity<BaseResponse<List<PendingUserResponse>>> getPendingUsers() {
+    public BaseResponse<List<PendingUserResponse>> getPendingUsers() {
         List<PendingUserResponse> pendingUsers = adminService.getPendingUsers();
-        return ResponseEntity.ok(new BaseResponse<>(pendingUsers));
+        return new BaseResponse<>(pendingUsers);
     }
 
-    @PutMapping("/approve/{userId}")
-    public ResponseEntity<?> approveUser(@PathVariable Long userId) {
-        adminService.approveUser(userId);
-        return ResponseEntity.ok("회원가입 승인 완료");
+    @PutMapping("/approve-users")
+    public BaseResponse<List<ApprovedUserResponse>> approveUsers(@RequestBody ApproveUsersRequest request) {
+        List<ApprovedUserResponse> approvedUserResponses = adminService.approveUsers(request.getUserLoginIds());
+        return new BaseResponse<>(approvedUserResponses);
     }
 }
