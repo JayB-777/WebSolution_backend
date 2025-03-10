28b3bec18fd0bec00937c8e4c1c888cae6224cf4
quswjdgns399 <quswjdgns3996@gmail.com>
2025-03-10 23:47:51 +0900

    feat: pending 상태의 유저 승인 후 반환 ResponseDto 구현

--- src/main/java/com/websolution/api/domains/admin/model/response/ApprovedUserResponse.java
+++ src/main/java/com/websolution/api/domains/admin/model/response/ApprovedUserResponse.java
@@ -1,5 +1,17 @@
 package com.websolution.api.domains.admin.model.response;
 
-public class AppovedUesrResponse {
+import com.websolution.api.domains.entity.Role;
+import com.websolution.api.domains.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApprovedUserResponse {
     private String loginId;
+    private Role role;
+
+    public static ApprovedUserResponse fromEntity(User user) {
+        return new ApprovedUserResponse(user.getLoginId(), user.getRole());
+    }
 }
