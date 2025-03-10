f3938f915a0246ad3293a920fe6831b03b9ab795
quswjdgns399 <quswjdgns3996@gmail.com>
2025-03-10 23:54:33 +0900

    feat: 로그인한 유저의 loginId를 반환하는 ResponseDto 구현

--- src/main/java/com/websolution/api/domains/users/model/response/UserLoginResponse.java
+++ src/main/java/com/websolution/api/domains/users/model/response/UserLoginResponse.java
@@ -1,2 +1,15 @@
-package com.websolution.api.domains.users.model.response;public class UserLoginResponse {
+package com.websolution.api.domains.users.model.response;
+
+import com.websolution.api.domains.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserLoginResponse {
+    private String loginId;
+
+    public static UserLoginResponse fromEntity(User user) {
+        return new UserLoginResponse(user.getLoginId());
+    }
 }
