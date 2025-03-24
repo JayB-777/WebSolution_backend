e3651b8b0422a2c974ce610bfaad21a8c2c72960
quswjdgns399 <quswjdgns3996@gmail.com>
2025-03-24 17:05:59 +0900

    feat: 로그인 응답에 accessToken, refreshToken, role 포함하도록 UserLoginResponse 확장

--- src/main/java/com/websolution/api/domains/users/model/response/UserLoginResponse.java
+++ src/main/java/com/websolution/api/domains/users/model/response/UserLoginResponse.java
@@ -1,5 +1,6 @@
 package com.websolution.api.domains.users.model.response;
 
+import com.websolution.api.domains.entity.Role;
 import com.websolution.api.domains.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,8 +9,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public class UserLoginResponse {
     private String loginId;
-
-    public static UserLoginResponse fromEntity(User user) {
-        return new UserLoginResponse(user.getLoginId());
+    private String accessToken;
+    private String refreshToken;
+    private Role role;
+    
+    public static UserLoginResponse fromEntity(User user, String accessToken, String refreshToken) {
+        return new UserLoginResponse(
+            user.getLoginId(),
+            accessToken,
+            refreshToken,
+            user.getRole()
+        );
     }
 }
